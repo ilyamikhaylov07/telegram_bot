b7cf13ea7bfc4a4f9637da18b82e84158c4203b2
Ilya <mr.ilmix@mail.ru>
2025-03-19 10:53:48 +0300

    update bot file

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import Update
-from telegram.ext import CallbackContext
+from telegram.ext import CallbackContext, filters,  MessageHandler, CommandHandler, Updater
 
 logging.basicConfig(format='%(ascitime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -22,4 +22,28 @@ def handle_text(update: Update, _: CallbackContext) -> None:
         user_message, bot_message = get_random_dialogue()
         update.message.reply_text(bot_message) 
 
+def handle_voice(update: Update, _: CallbackContext) -> None:
+    voice = update.message.voice
+    voice_file = voice.get_file()
+    voice_file.download('voice_message.ogg')
+
+    handle_voice_command('voice_message.ogg')
+    update.message.reply_text("Я обработал ваш голосовой запрос")
+
+def main() -> None:
+
+    token = 'YOUR_BOT_TOKEN'
+
+    updater = Updater(token, use_context=True)
     
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_text))
+    dispatcher.add_handler(MessageHandler(filters.VOICE, handle_voice))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
